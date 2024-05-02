3894aceec7cfa2021cf0057f3562943d09b003ed
SURAJ GUPTA <137384631+shekhar-sg@users.noreply.github.com>
2024-05-02 11:03:08 +0530

    1. Removed unused imports and refactored    props in several components. 2. In CurrentWeatherCard, the date is now    calculated with respect to the timezone    data received from API. 3. In CityPage, removed hard-coded    ForecastWeatherSwiper mock data.

--- src/app/[coordinates]/page.tsx
+++ src/app/[coordinates]/page.tsx
@@ -1,61 +1,59 @@
 "use client";
 import SectionWrapper from "@/commonComponents/section-wrapper";
 import CurrentWeatherCard from "@c/homePage/current-weather-card";
-import ForecastWeatherSwiper, {
-  ForecastWeatherSwiperProps,
-} from "@c/homePage/forecast-weather-swiper";
-import { useEffect } from "react";
-import { useAppConfig } from "@c/layout";
+import ForecastWeatherSwiper from "@c/homePage/forecast-weather-swiper";
+import {useEffect} from "react";
+import {useAppConfig} from "@c/layout";
 
 interface CityPageProps {
-  params: {
-    coordinates: string;
-  };
+    params: {
+        coordinates: string;
+    };
 }
 
 const CityPage = (props: CityPageProps) => {
-  const { params } = props;
-  const { coordinates, setCoordinates } = useAppConfig();
-  useEffect(() => {
-    const [lat, lon] = params.coordinates.split("%2C");
-    setCoordinates({
-      lat: parseFloat(lat),
-      lon: parseFloat(lon),
-    });
-  }, [params.coordinates, setCoordinates]);
-  return (
-    <SectionWrapper
-      direction={{
-        xs: "column",
-        lg: "row",
-      }}
-      mt={'10%'}
-      spacing={2}
-      alignItems={{ lg: "flex-end" }}
-      justifyContent={"space-between"}
-    >
-      {coordinates ? (
-        <>
-          <CurrentWeatherCard
-            width={{
-              xs: 1,
-              lg: 0.4,
+    const {params} = props;
+    const {coordinates, setCoordinates} = useAppConfig();
+    useEffect(() => {
+        const [lat, lon] = params.coordinates.split("%2C");
+        setCoordinates({
+            lat: parseFloat(lat),
+            lon: parseFloat(lon),
+        });
+    }, [params.coordinates, setCoordinates]);
+    return (
+        <SectionWrapper
+            direction={{
+                xs: "column",
+                lg: "row",
             }}
-          />
-          <ForecastWeatherSwiper
-            SwiperProps={{
-              sx: {
-                width: 1,
-                maxWidth: {
-                  lg: 0.6,
-                },
-              },
-            }}
-          />
-        </>
-      ) : null}
-    </SectionWrapper>
-  );
+            mt={'10%'}
+            spacing={2}
+            alignItems={{lg: "flex-end"}}
+            justifyContent={"space-between"}
+        >
+            {coordinates ? (
+                <>
+                    <CurrentWeatherCard
+                        width={{
+                            xs: 1,
+                            lg: 0.4,
+                        }}
+                    />
+                    <ForecastWeatherSwiper
+                        SwiperProps={{
+                            sx: {
+                                width: 1,
+                                maxWidth: {
+                                    lg: 0.6,
+                                },
+                            },
+                        }}
+                    />
+                </>
+            ) : null}
+        </SectionWrapper>
+    );
 };
 
 export default CityPage;
